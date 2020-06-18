f9c90c6ed20f6af7c99fe51d353aaa9d482c21b2
nrafia <nrafia@gmail.com>
2020-06-18 19:02:29 +0400

    Version 1.01

--- properties/admin.py
+++ properties/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from .models import Property, States, Reserve
 
-admin.site.register(Property)
 admin.site.register(States)
 admin.site.register(Reserve)
+
+
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'state', 'price', 'ads_id', 'featured')
+    ordering = ('name',)
+    search_fields = ('ads_id',)
+
+
+admin.site.register(Property, PropertyAdmin)
